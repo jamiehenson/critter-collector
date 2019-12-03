c22167727de3fa8c8e5454643816a81b29b9701f
Jamie Henson <jamie@jamiehenson.com>
2019-12-03 17:26:58 +0000

    Updates

---
+++ precache-manifest.015d5acb4bb39ca043945a50736d936f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40fe2e4a69c16e6972a11199055c28d3",
+    "url": "/critter-collector/index.html"
+  },
+  {
+    "revision": "e3c0a4a83b742e9de5b9",
+    "url": "/critter-collector/static/css/main.25723443.chunk.css"
+  },
+  {
+    "revision": "f92c0ad830ed57c68082",
+    "url": "/critter-collector/static/js/2.59e6b564.chunk.js"
+  },
+  {
+    "revision": "e3c0a4a83b742e9de5b9",
+    "url": "/critter-collector/static/js/main.2f45d222.chunk.js"
+  },
+  {
+    "revision": "09a01cdc54f6885c9cbe",
+    "url": "/critter-collector/static/js/runtime-main.df8aba61.js"
+  },
+  {
+    "revision": "8eb8a5fb53ccbedccfa5620e3b605386",
+    "url": "/critter-collector/static/media/grass.8eb8a5fb.png"
+  },
+  {
+    "revision": "320ecf95ecdd544a3e18f41ccd91762f",
+    "url": "/critter-collector/static/media/sand.320ecf95.png"
+  },
+  {
+    "revision": "879caf56aa40afbd397d976f2244d5e7",
+    "url": "/critter-collector/static/media/water.879caf56.png"
+  }
+]);
